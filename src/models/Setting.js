98d9a29fd6a851f44efdf66f003bf2ae65aecd4d
Abner <abner.melo400@gmail.com>
2026-02-10 22:22:53 -0300

    feat: escala semanal 7 dias default

--- src/models/Setting.js
+++ src/models/Setting.js
@@ -16,6 +16,25 @@ const settingSchema = new mongoose.Schema({
         active: { type: Boolean, default: true }
     },
     workingDays: { type: [Number], default: [1, 2, 3, 4, 5] }, // 0=Sun, 1=Mon...
+    weeklySchedule: {
+        type: [{
+            day: { type: Number }, // 0=Sun, 1=Mon...
+            active: { type: Boolean, default: true },
+            period1Start: { type: String, default: '09:00' },
+            period1End: { type: String, default: '12:00' },
+            period2Start: { type: String, default: '13:00' },
+            period2End: { type: String, default: '18:00' }
+        }],
+        default: () => [
+            { day: 0, active: false, period1Start: '09:00', period1End: '12:00', period2Start: '13:00', period2End: '18:00' },
+            { day: 1, active: true, period1Start: '09:00', period1End: '12:00', period2Start: '13:00', period2End: '18:00' },
+            { day: 2, active: true, period1Start: '09:00', period1End: '12:00', period2Start: '13:00', period2End: '18:00' },
+            { day: 3, active: true, period1Start: '09:00', period1End: '12:00', period2Start: '13:00', period2End: '18:00' },
+            { day: 4, active: true, period1Start: '09:00', period1End: '12:00', period2Start: '13:00', period2End: '18:00' },
+            { day: 5, active: true, period1Start: '09:00', period1End: '12:00', period2Start: '13:00', period2End: '18:00' },
+            { day: 6, active: true, period1Start: '09:00', period1End: '12:00', period2Start: '13:00', period2End: '18:00' }
+        ]
+    },
     slotDuration: { type: Number, default: 30 }, // minutes
     closedDays: [{ type: Date }], // Specific dates where the shop is closed
     founderImage: { type: String, default: '' }, // URL of the founder's image
